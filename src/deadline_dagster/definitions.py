7c503dbf7b5635913c13c3fe0a048a24dfef6473
Michael Mussato <michimussato@gmail.com>
2025-01-23 14:31:04 +0100

    initial commit

---
+++ src/deadline_dagster/definitions.py
@@ -0,0 +1,44 @@
+import os
+
+from dagster import Definitions, load_assets_from_modules, AutoMaterializePolicy, AutoMaterializeRule
+
+from deadline_dagster.assets import read_yaml, submit_jobs, constants_factory
+from deadline_dagster.resources import (
+    KitsuResource,
+)
+from deadline_dagster.sensors import submission_sensor, ingestion_sensor, my_custom_auto_materialize_sensor
+
+
+read_yaml_assets = load_assets_from_modules(
+    modules=[read_yaml],
+    auto_materialize_policy=AutoMaterializePolicy.lazy().with_rules(
+            AutoMaterializeRule.materialize_on_parent_updated(),
+    )
+)
+submit_jobs_assets = load_assets_from_modules([submit_jobs])
+
+
+all_sensors = [submission_sensor, ingestion_sensor, my_custom_auto_materialize_sensor]
+
+
+resources = {
+    "local": {
+        "kitsu_resource": KitsuResource(),
+    },
+    # "staging": {
+    #     "kitsu_resource": KitsuResource(),
+    # },
+    "farm": {
+        "kitsu_resource": KitsuResource(),
+    },
+}
+
+
+deployment_name = os.getenv("DAGSTER_DEPLOYMENT", "farm")
+
+
+defs = Definitions(
+    assets=[*read_yaml_assets, *submit_jobs_assets, *constants_factory.assets],
+    resources=resources[deployment_name],
+    sensors=all_sensors,
+)
